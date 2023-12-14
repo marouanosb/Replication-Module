f1154f9dfa34d125193d8b4cc364133b161f4e5b
unknown <marouanosb@gmail.com>
2023-12-14 18:32:12 +0100

    initial commit

---
+++ replc.py
@@ -0,0 +1,10 @@
+import time
+
+# 'i' stands for the number of times we want to replicate the experience
+# 'j' stands for the number of times we want to repeat the funtion
+# 'func' stands for the function we want to replicate
+# 'args' is the list of arguments the are passed when calling the function call
+def replicate(i, j, func, args):
+    results = []
+
+    return resuls
